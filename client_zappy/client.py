3ac2a9a5e6c4125cf1240bbaa806985f53488d58
uvillanueva <ulysse.villanueva@epitech.eu>
2023-05-25 14:10:18 +0200

    [ADD] args pparsing

---
+++ client_zappy/client.py
@@ -0,0 +1,6 @@
+
+class Client:
+    def __init__(self, port, team, machine):
+        self.port = port
+        self.team = team,
+        self.machine = machine
---
+++ client_zappy/main.py
@@ -0,0 +1,36 @@
+import sys
+import getopt
+from client import Client
+
+def print_usage():
+    print("USAGE:\t./zappy_ai -p port -n name -h machine"
+          "\n\tport\tis the port number"
+          "\n\tname\tis the name of the team"
+          "\n\tmachine\tis the name of the machine; localhost by default")
+
+def main():
+    argv = sys.argv[1:]
+    port = None
+    name = None
+    machine = "localhost"
+
+    try:
+        opts, args = getopt.getopt(argv, "p:n:h:help")
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(84)
+    
+    for opt, arg in opts:
+        if opt in ["-help"]:
+            print_usage()
+            exit(0)
+        elif opt in ['-p']:
+            port = int(arg)
+        elif opt in ['-n']:
+            name = arg
+        elif opt in ['-h']:
+            machine = arg
+    if port is None or name is None:
+        print_usage()
+        sys.exit(84)
+    client = Client(port, name, machine)
