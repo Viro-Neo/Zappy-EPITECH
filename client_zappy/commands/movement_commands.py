ad0ab9c5b24f75a02bbc861bf1894fc7351eccdd
Aurelien Duval <aurelien.duval@epitech.eu>
2023-05-31 13:36:31 +0200

    [ADD] Commands

--- client_zappy/commands/movement_commands.py
+++ client_zappy/commands/movement_commands.py
@@ -1,7 +1,76 @@
 from client import Client
 
 def send_forward_command(client):
-    command = "USER "
+    command = "Forward"
+    client.write_response_to_socket(command)
+
+    try:
+        response = client.receive_server_response()
+        if response == "ok" or response == "ko":
+            print(response)
+        else:
+            print(f"Unknown server response: {response}")
+    except OSError as e:
+        print(f"Error receiving response: {str(e)}")
+
+
+def send_right_command(client):
+    command = "Right"
+    client.write_response_to_socket(command)
+
+    try:
+        response = client.receive_server_response()
+        if response == "ok" or response == "ko":
+            print(response)
+        else:
+            print(f"Unknown server response: {response}")
+    except OSError as e:
+        print(f"Error receiving response: {str(e)}")
+
+def send_left_command(client):
+    command = "Left"
+    client.write_response_to_socket(command)
+
+    try:
+        response = client.receive_server_response()
+        if response == "ok" or response == "ko":
+            print(response)
+        else:
+            print(f"Unknown server response: {response}")
+    except OSError as e:
+        print(f"Error receiving response: {str(e)}")
+
+
+def send_look_command(client):
+    command = "Look"
+    client.write_response_to_socket(command)
+
+    try:
+        response = client.receive_server_response()
+        if response == "ok" or response == "ko":
+            print(response)
+        else:
+            print(f"Unknown server response: {response}")
+    except OSError as e:
+        print(f"Error receiving response: {str(e)}")
+
+
+def send_inventory_command(client):
+    command = "Inventory"
+    client.write_response_to_socket(command)
+
+    try:
+        response = client.receive_server_response()
+        if response == "ok" or response == "ko":
+            print(response)
+        else:
+            print(f"Unknown server response: {response}")
+    except OSError as e:
+        print(f"Error receiving response: {str(e)}")
+
+
+def send_broadcast_text_command(client):
+    command = "Broadcast text"
     client.write_response_to_socket(command)
 
     try:
