1d79844f18933f0159b223fe6cbb6d260e8c8878
Zacharie Lacombe <zacharielacombe@pop-os.localdomain>
2023-06-07 12:59:59 +0200

    [ADD]: adding basic networking encapsulation

--- gui_zappy/include/ServerCommunication.hpp
+++ gui_zappy/include/ServerCommunication.hpp
@@ -11,16 +11,32 @@
     #include <signal.h>
     #include <errno.h>
     #include <netdb.h>
+    #include <string>
+    #include <memory>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include "HostentByName.hpp"
+    #include <vector>
 class ServerCommunication {
     public:
-        ServerCommunication();
+        ServerCommunication(std::string port, std::string host);
         ~ServerCommunication();
         int readFromServer();
         int writeToServer();
+        void connectToServer(void);
+        void selectHandler(void);
+        //return true if u need to read 0 if not
+        bool isRead();
     protected:
     private:
-        sockaddr_in client_address;
-        hostent info;
+        struct sockaddr_in _client_address;
+        HostentByName _info;
+        int _client_socket;  
+        const std::string _port;
+        const std::string _host;
+        fd_set _fdset;
+        std::vector<std::string> _cmdList;
 };
 
 
--- gui_zappy/src/ServerCommunication.cpp
+++ gui_zappy/src/ServerCommunication.cpp
@@ -7,22 +7,74 @@
 
 #include "ServerCommunication.hpp"
 
-ServerCommunication::ServerCommunication()
+
+ServerCommunication::ServerCommunication(const std::string port, const std::string host) : _port(port), _host(host), _info(HostentByName(host))
 {
-    
+    this->_client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    this->_client_address.sin_addr.s_addr = inet_addr(host.data());
+    this->_client_address.sin_family = AF_INET;
+    this->_client_address.sin_port = htons(atoi(port.data()));
+    // TODO(zach & vando): make logs
 }
+
 
 ServerCommunication::~ServerCommunication()
 {
+    close(this->_client_socket);
+}
 
+void ServerCommunication::connectToServer(void)
+{
+    if (connect(this->_client_socket, (struct sockaddr *)&this->_client_address, sizeof(_client_address)) == -1)
+        throw std::exception(); //TODO(zach): make error handling 
+    //else
+    //  TODO(zach & vando): make logs
+}
+
+void ServerCommunication::selectHandler(void)
+{
+    FD_ZERO(&this->_fdset);
+    FD_SET(STDIN_FILENO, &this->_fdset);
+    FD_SET(this->_client_socket, &this->_fdset);
+    if (select(this->_client_socket + 1, &this->_fdset, nullptr, nullptr, nullptr) == -1 && errno != EINTR)
+        throw std::exception(); //TODO(zach): make error handling
+}
+
+bool ServerCommunication::isRead()
+{
+    if (FD_ISSET(STDIN_FILENO, &this->_fdset))
+        return false;
+    else if (FD_ISSET(this->_client_socket, &this->_fdset))
+        return true;
 }
 
 int ServerCommunication::readFromServer()
 {
-    
+    char msg[4096];
+    int status = recv(this->_client_socket, msg, 4096, 0);
+    if (status = 0) {
+        this->~ServerCommunication();
+        throw std::exception(); //TODO(zach): make error handling
+    }
+    if (status = -1) { 
+        throw std::exception(); //TODO(zach): make error handling
+    }
+    std::string result = msg;
+    int size = 0;
+    while (result != "" || result.find('\n') == std::string::npos) {
+        int index = result.find('\n', size);
+        this->_cmdList.push_back(result.substr(size, index - 1));
+        size = index + 1;
+    }
 }
 
 int ServerCommunication::writeToServer()
 {
-    
+    char msg[1024] = {0};
+    read(STDIN_FILENO, msg, 1024 - 1);
+
+    write(this->_client_socket, msg, 1024);
+    return 0;
 }
+
+
