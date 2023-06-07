58302afb785a15644895fb4ae03da5b6033061ee
Zacharie Lacombe <zacharielacombe@pop-os.localdomain>
2023-06-07 16:46:37 +0200

    [ADD] error class and subclass

--- gui_zappy/include/Errors.hpp
+++ gui_zappy/include/Errors.hpp
@@ -10,62 +10,31 @@
 
 #include <iostream>
 
-class Errors {
+class Errors : public std::exception {
+    private:
     public:
-        class NotEnoughArgumentsError : public std::exception {
+        
+        
+        class NotEnoughArgumentsError {
             private:
                 const std::string message = "Not enough arguments";
             public:
                 const char *what() const throw();
+                
         };
-        class ArgumentNotANumber : public std::exception {
+        class ArgumentNotANumber {
             private:
                 const std::string message = "Argument is not a number";
             public:
                 const char *what() const throw();
         };
-        class ArgumentError : public std::exception {
+        class ArgumentError {
             private:
                 const std::string message = "Only first argument should be a float or too much '.' in argument";
             public:
                 const char *what() const throw();
         };
-        class PizzaTypeError : public std::exception {
-            private:
-                const std::string message = "Wrong pizza type";
-            public:
-                const char *what() const throw();
-        };
-        class PizzaSizeError : public std::exception {
-            private:
-                const std::string message = "Wrong pizza size";
-            public:
-                const char *what() const throw();
-        };
-        class PizzaNumberError : public std::exception {
-            private:
-                const std::string message = "Wrong pizza number";
-            public:
-                const char *what() const throw();
-        };
-        class PipeOpenError : public std::exception {
-            private:
-                const std::string message = "Pipe coulnd't open right";
-            public:
-                const char *what() const throw();
-        };
-        class PipeWrongHandling : public std::exception {
-            private:
-                const std::string message = "Pipe function used wrongly";
-            public:
-                const char *what() const throw();
-        };
-        class PipeReadError : public std::exception {
-            private:
-                const std::string message = "Reading Error occured";
-            public:
-                const char *what() const throw();
-        };
+        
 };
 
 #endif
--- gui_zappy/src/Gui.cpp
+++ gui_zappy/src/Gui.cpp
@@ -6,10 +6,13 @@
 */
 
 #include "Gui.hpp"
+#include "Errors.hpp"
 #include "GetOpt.hpp"
+
 
 Gui::Gui(int ac, char **av)
 {
+    
     if (ac == 2 && std::string(av[1]) == "-help")
         throw std::invalid_argument("Help");
     GetOpt getOpt(ac, av);
