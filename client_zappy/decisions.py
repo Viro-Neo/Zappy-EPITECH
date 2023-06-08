c2b17fabc510f5ea91e089d773cbb01663a17101
uvillanueva <ulysse.villanueva@epitech.eu>
2023-06-08 13:43:03 +0200

    [ADD] added decision functions (empty for now)

---
+++ client_zappy/decisions.py
@@ -0,0 +1,46 @@
+from client import Client
+
+def decide_forward(client: Client, response: str):
+    pass
+
+def decide_right(client: Client, response: str):
+    pass
+
+def decide_left(client: Client, response: str):
+    pass
+
+def decide_look(client: Client, response: str):
+    print("Should make decisions based on what I see")
+
+def decide_inventory(client: Client, response: str):
+    print("Should make decision based on inventory")
+
+def decide_broadcast(client: Client, response: str):
+    pass
+
+def decide_connect_nbr(client: Client, response: str):
+    pass
+
+def decide_fork(client: Client, response: str):
+    pass
+
+def decide_eject(client: Client, response: str):
+    pass
+
+def decide_take(client: Client, response: str):
+    pass
+
+def decide_set(client: Client, response: str):
+    pass
+
+def decide_incantation(client: Client, response: str):
+    pass
+
+def died(client: Client, response: str):
+    pass
+
+def decide_heard(client: Client, response: str):
+    print("Should make decision based on what i heard")
+
+def decide_ejected(client: Client, response: str):
+    print("Should make decision after being ejected")
