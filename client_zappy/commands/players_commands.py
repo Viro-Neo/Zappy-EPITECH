9ba833fcb3dc7431873378d21d2c81f6577a3dea
uvillanueva <ulysse.villanueva@epitech.eu>
2023-06-08 13:55:36 +0200

    [FIX] no longer waits for a response after sending a command

--- client_zappy/commands/players_commands.py
+++ client_zappy/commands/players_commands.py
@@ -4,37 +4,10 @@ def send_connect_nbr_command(client):
     command = "Connect_nbr"
     client.write_response_to_socket(command)
 
-    try:
-        response = client.receive_server_response()
-        if response.isnumeric():
-            print(response)
-        else:
-            print(f"Unknown server response: {response}")
-    except OSError as e:
-        print(f"Error receiving response: {str(e)}")
-
 def send_fork_command(client):
     command = "Fork"
     client.write_response_to_socket(command)
 
-    try:
-        response = client.receive_server_response()
-        if response == "ok":
-            print(response)
-        else:
-            print(f"Unknown server response: {response}")
-    except OSError as e:
-        print(f"Error receiving response: {str(e)}")
-
 def send_eject_command(client):
     command = "Eject"
     client.write_response_to_socket(command)
-
-    try:
-        response = client.receive_server_response()
-        if response == "ok" or response == "ko":
-            print(response)
-        else:
-            print(f"Unknown server response: {response}")
-    except OSError as e:
-        print(f"Error receiving response: {str(e)}")
