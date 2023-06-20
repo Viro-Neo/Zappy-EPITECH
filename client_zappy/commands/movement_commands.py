b37fb60f7e55432d215380d8e9c10da7d2880577
Aurelien Duval <aurelien.duval@epitech.eu>
2023-06-20 14:58:59 +0200

    [FIX] Take decision function

--- client_zappy/commands/movement_commands.py
+++ client_zappy/commands/movement_commands.py
@@ -54,43 +54,6 @@ def send_look_command(client: Client):
     command = "Look"
     client.write_response_to_socket(command)
 
-    try:
-        response = "stone, stone, item,,,,,,,"  # client.receive_server_response()
-        if response == "ok" or response == "ko":
-            print(response)
-        else:
-            response_list = [x.strip() for x in response.split(',')]
-            print(response_list)
-            nearest_food = None
-            nearest_distance = float('inf')
-            starting_position = response_list.index("stone")
-
-            print("I go forward first of all")
-            send_forward_command(client)
-
-            for index, tile in enumerate(response_list):
-                if tile in client.missing:
-                    distance = abs(starting_position - index)
-                    if distance < nearest_distance:
-                        nearest_food = index
-                        nearest_distance = distance
-
-            if nearest_food is not None:
-                if nearest_food < starting_position:
-                    send_left_command(client)
-                    print("I go left")
-                elif nearest_food > starting_position:
-                    send_forward_command(client)
-                    print("I go forward")
-                else:
-                    print("I go right")
-                    send_right_command(client)
-
-    except OSError as e:
-        print(f"Error receiving response: {str(e)}")
-
-
-
 
 def send_inventory_command(client: Client) -> str:
     command = "Inventory"
