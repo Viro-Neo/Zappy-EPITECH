8bbcff02374c0a920453d75609d3a2004cb7dff9
Van-Do PHAM <van-do.pham@epitech.eu>
2023-06-22 20:14:26 +0200

    [ADD] each function to manage the arguments, will complete later

--- gui_zappy/src/FunctionManager.cpp
+++ gui_zappy/src/FunctionManager.cpp
@@ -9,30 +9,29 @@
 
 FunctionManager::FunctionManager()
 {
-    _fct["msz"] = &Map::setMapSize;
-    _fct["bct"] = &Map::setTileContent;
-    _fct["tna"] = &Map::setTeamName;
-    _fct["pnw"] = &Map::addPlayer;
-    _fct["ppo"] = &Map::setPlayerPosition;
-    _fct["plv"] = &Map::setPlayerLevel;
-    _fct["pin"] = &Map::setPlayerInventory;
-    _fct["pex"] = &Map::playerExpulse;
-    _fct["pbc"] = &Map::playerBroadcast;
-    _fct["pic"] = &Map::playerIncantation;
-    _fct["pie"] = &Map::playerIncantationEnd;
-    _fct["pfk"] = &Map::playerFork;
-    _fct["pdr"] = &Map::playerDropRessource;
-    _fct["pgt"] = &Map::playerTakeRessource;
-    _fct["pdi"] = &Map::playerDie;
-    _fct["enw"] = &Map::eggLaying;
-    _fct["eht"] = &Map::eggHatching;
-    _fct["ebo"] = &Map::eggBroken;
-    _fct["edi"] = &Map::eggDie;
-    _fct["sgt"] = &Map::setTimeUnit;
-    _fct["seg"] = &Map::endGame;
-    _fct["smg"] = &Map::serverMessage;
-    _fct["suc"] = &Map::unknownCommand;
-    _fct["sbp"] = &Map::badParameter;
+    _fct["msz"] = &msz;
+    _fct["bct"] = &bct;
+    _fct["tna"] = &tna;
+    _fct["pnw"] = &pnw;
+    _fct["ppo"] = &ppo;
+    _fct["plv"] = &plv;
+    _fct["pin"] = &pin;
+    _fct["pex"] = &pex;
+    _fct["pbc"] = &pbc;
+    _fct["pic"] = &pic;
+    _fct["pie"] = &pie;
+    _fct["pfk"] = &pfk;
+    _fct["pdr"] = &pdr;
+    _fct["pgt"] = &pgt;
+    _fct["pdi"] = &pdi;
+    _fct["enw"] = &enw;
+    _fct["ebo"] = &ebo;
+    _fct["edi"] = &edi;
+    _fct["sgt"] = &sgt;
+    _fct["seg"] = &seg;
+    _fct["smg"] = &smg;
+    _fct["suc"] = &suc;
+    _fct["sbp"] = &sbp;
 }
 
 void FunctionManager::callFunction(std::string command, Map myMap)
@@ -54,4 +53,136 @@ std::map<std::string, void (*)(std::list<std::string> arg, Map myMap)> FunctionM
 void FunctionManager::setFct(const std::map<std::string, void (*)(std::list<std::string> arg, Map myMap)> &fct)
 {
     _fct = fct;
+}
+
+void FunctionManager::msz(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 2)
+        return;
+}
+
+void FunctionManager::bct(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 9)
+        return;
+}
+
+void FunctionManager::tna(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::pnw(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 6)
+        return;
+}
+
+void FunctionManager::ppo(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 4)
+        return;
+}
+
+void FunctionManager::plv(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 2)
+        return;
+}
+
+void FunctionManager::pin(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 10)
+        return;
+}
+
+void FunctionManager::pex(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::pbc(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 2)
+        return;
+}
+
+void FunctionManager::pic(std::list<std::string> arg, Map &myMap)
+{
+}
+
+void FunctionManager::pie(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 3)
+        return;
+}
+
+void FunctionManager::pfk(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::pdr(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 2)
+        return;
+}
+
+void FunctionManager::pgt(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 2)
+        return;
+}
+
+void FunctionManager::pdi(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::enw(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 4)
+        return;
+}
+
+void FunctionManager::ebo(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::edi(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::sgt(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::seg(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::smg(std::list<std::string> arg, Map &myMap)
+{
+    if (arg.size() != 1)
+        return;
+}
+
+void FunctionManager::suc(std::list<std::string> arg, Map &myMap)
+{
+}
+
+void FunctionManager::sbp(std::list<std::string> arg, Map &myMap)
+{
 }
