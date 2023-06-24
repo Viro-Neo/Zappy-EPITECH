9ea63db08afb4fc7968698de12ba496201083d51
uvillanueva <ulysse.villanueva@epitech.eu>
2023-06-24 12:13:20 +0200

    [FIX] now using correct amount of parameters for inventory functions in client

--- client_zappy/inventory.py
+++ client_zappy/inventory.py
@@ -79,10 +79,7 @@ def init_inventory() -> Dict[str, int]:
 
     return inventory
 
-def get_inventory(client) -> Dict[str, int]:
-    # get string describing inventory from server
-    inventory_str = send_inventory_command(client)
-
+def get_inventory(client, inventory_str: str) -> Dict[str, int]:
     # we initialize a dict to store the items
     inventory = init_inventory()
 
@@ -100,9 +97,9 @@ def get_inventory(client) -> Dict[str, int]:
     
     return inventory
 
-def check_inventory(client):
+def check_inventory(client, response: str):
     # get the inventory
-    inventory = get_inventory(client)
+    inventory = get_inventory(client, response)
 
     missing = {}
 
